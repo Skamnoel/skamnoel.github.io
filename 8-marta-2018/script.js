2bcf47c3f0d7202627f99415119a0dc1196ef039
Skamnoel <elfonok@mail.ru>
2018-03-06 00:50:51 +0300

    наброски для поздравления с 8 марта

--- 8-marta-2018/script.js
+++ 8-marta-2018/script.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     var flippers = $('.flip-container');
-    function handler(event){
-        this.classList.toggle('hover');
+    function handler(debugString){
+        return function (event) {
+            console.log(debugString);
+            $(this).closest('.flip-container').toggleClass('hover');
+            this.classList.toggle('hover');
+        }
     }
-    flippers.bind('mouseenter', handler);
-    flippers.bind('mouseleave', handler);
-    flippers.bind('touchstart', handler);
+    // flippers.find('.front').bind('touchstart', handler('touchstartFront'));
+    // flippers.find('.back').bind('touchstart', handler('touchstartBack'));
+    flippers.find('.front').bind('click', handler('clickFront'));
+    flippers.find('.back').bind('click', handler('clickBack'));
 });
